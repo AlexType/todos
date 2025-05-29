e87caf914835f0677d27964d1f09dc9ad71d6368
Alexand Bryzgalov <bryzgalov@gnivc.ru>
2025-05-29 12:43:16 +1000

    feat: настроен TaskForm

--- src/components/TaskForm/TaskForm.tsx
+++ src/components/TaskForm/TaskForm.tsx
@@ -1,22 +1,54 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import classes from "./TaskForm.module.scss";
-import Input from "antd/es/input/Input";
-import EnterOutlined from "@ant-design/icons/EnterOutlined";
+import Input from 'antd/es/input';
+import Button from 'antd/es/button';
+import EnterOutlined from '@ant-design/icons/EnterOutlined';
+import { Tooltip } from 'antd';
 
-const TaskForm = () => {
-  const [text, setText] = useState("");
+type Props = {
+  onCreate?: (title: string) => void;
+};
+
+const TaskForm: React.FC<Props> = ({ onCreate }) => {
+  const [text, setText] = useState('');
+  const isDisabledAction = !text.trim().length;
+
+  function createHandler() {
+    if (!onCreate) {
+      return;
+    }
+
+    onCreate(text);
+    setText('');
+  }
+
+  function keyDownHandler(event: React.KeyboardEvent) {
+    if (event.key === 'Enter' && (event.metaKey || event.ctrlKey)) {
+      createHandler();
+    }
+  }
 
   return (
-    <div className={classes.container}>
-      <Input
-        value={text}
-        size='large'
-        placeholder="Введите новую задачу"
-        onChange={(e) => setText(e.target.value)}
-        prefix={<EnterOutlined />}
-      />
-    </div>
+    <Input
+      value={text}
+      size="large"
+      showCount
+      maxLength={500}
+      placeholder="Введите новую задачу"
+      onChange={(e) => setText(e.target.value)}
+      onKeyDown={keyDownHandler}
+      suffix={
+        <Tooltip placement="top" title="Создать задачу">
+          <Button
+            shape="circle"
+            variant="outlined"
+            icon={<EnterOutlined />}
+            disabled={isDisabledAction}
+            onClick={createHandler}
+          />
+        </Tooltip>
+      }
+    />
   );
 };
 
